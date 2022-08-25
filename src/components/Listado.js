d4f18d779e40e49a71b880b31e01450d04aad385
adrianlunau <adrianelunau@gmail.com>
2022-08-25 20:45:24 -0300

    Entregable terminado

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Item from './Item'
 
 // Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
 // El componente Listado es el padre de:
@@ -7,10 +8,14 @@ import React from 'react'
 // MÉTODOS: Listado no requiere de métodos.
 // PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
 
-export default function Listado() {
+const datos = require('./data.json')
+
+export default function Listado({func}) {
   return (
     <div className='container'>
-      {/* renderizamos los Item aquí */}
+      {datos.map((p, index) => (
+        <Item key={index} product={p} func={func}/>
+      ))}
     </div>
   )
 }
